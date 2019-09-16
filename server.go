f0321aa429ee5722e22de262a2f6fbd47bcaccb8
Bombwhale <solkblte@icloud.com>
2019-09-16 23:58:16 +0900

    Support TLS

--- server.go
+++ server.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"os"
+
 	"github.com/labstack/echo"
 	"golang.org/x/crypto/acme/autocert"
 )
 
 func autoCertConfig(e *echo.Echo) {
 	e.AutoTLSManager.HostPolicy = autocert.HostWhitelist(Namespace)
-	e.AutoTLSManager.Cache = autocert.DirCache("~/server/fespay/.cache")
+	e.AutoTLSManager.Cache = autocert.DirCache("~/sites/fespay/.cache")
 }
 
 func run() {
 	e := echo.New()
 	routes(e)
-	if err := e.Start(":8080"); err != nil {
-		panic(err)
+
+	sslMode := os.Getenv("SSL_MODE")
+	if sslMode == "AUTO" {
+		autoCertConfig(e)
+		e.Logger.Fatal(e.StartAutoTLS(":443"))
+	} else if sslMode == "MANUAL" {
+		e.Logger.Fatal(e.StartTLS(":443", os.Getenv("SSL_CRT"), os.Getenv("SSL_PRI")))
+	} else {
+		e.Logger.Fatal(e.Start(":80"))
 	}
 }
