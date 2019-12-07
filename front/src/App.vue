3b80847cb87fa52b430050afecedd6de2c25b19c
Bombwhale <solkblte@icloud.com>
2019-12-07 19:28:02 +0900

    front-end -> front working

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Vue.js에 오신 걸 환영합니다"/>
+    <h1>Hello App!</h1>
+    <p>
+      <!-- 네비게이션을 위해 router-link 컴포넌트를 사용합니다. -->
+      <!-- 구체적인 속성은 `to` prop을 이용합니다. -->
+      <!-- 기본적으로 `<router-link>`는 `<a>` 태그로 렌더링됩니다.-->
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+    </p>
+    <!-- 라우트 아울렛 -->
+    <!-- 현재 라우트에 맞는 컴포넌트가 렌더링됩니다. -->
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'App',
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
