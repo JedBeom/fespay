552316f71a196c9d9d789b5b3e4492d5196a1ec2
Bombwhale <solkblte@icloud.com>
2019-12-09 00:10:01 +0900

    vue router example

--- front/src/main.js
+++ front/src/main.js
@@ -1,22 +1,8 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import App from './App.vue'
-Vue.use(VueRouter)
+import router from './router'
 
 Vue.config.productionTip = false
-
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
-const routes = [
-  { path: '/', component: Foo },
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
-]
-
-const router = new VueRouter({
-  routes // `routes: routes`의 줄임
-})
 
 new Vue({
   router,
